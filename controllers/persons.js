1cf2cc4d9fc620147e05b0770b918fc3c2fb38c3
AndresBo <anndresfborrero@gmail.com>
2023-07-07 20:09:13 +1000

    build: moved route handlers to controllers/persons.js

---
+++ controllers/persons.js
@@ -0,0 +1,74 @@
+const personsRouter = require('express').Router()
+const Person = require('../models/person')
+
+
+personsRouter.get('/', (request, response) => {
+  Person.find({}).then(person => {
+    response.json(person)
+  })
+})
+
+personsRouter.get('/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })// next function needs a parameter(error) to continue to errorHandler middleware:
+    .catch(error => next(error))
+})
+
+personsRouter.delete('/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+personsRouter.post('/', (request, response, next) => {
+  // get new person from request object
+  const body = request.body
+  // create new person object using Note constructor function
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  // add to persons array
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+personsRouter.put('/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+personsRouter.get('/info', (request, response, next) => {
+  Person.count()
+    .then(count => {
+      response.json(
+        { NumberOfPersons:count,
+          date:Date()
+        }
+      )
+    })
+    .catch((error) => next(error) )
+})
+
+
+module.exports = personsRouter
--- index.js
+++ index.js
@@ -62,79 +62,80 @@ app.use(requestLogger)
 // }
 
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(person => {
-    response.json(person)
-  })
-})
+// app.get('/api/persons', (request, response) => {
+//   Person.find({}).then(person => {
+//     response.json(person)
+//   })
+// })
 
 
-app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-    .then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-    })// next function needs a parameter(error) to continue to errorHandler middleware:
-    .catch(error => next(error))
-})
+// app.get('/api/persons/:id', (request, response, next) => {
+//   Person.findById(request.params.id)
+//     .then(person => {
+//       if (person) {
+//         response.json(person)
+//       } else {
+//         response.status(404).end()
+//       }
+//     })// next function needs a parameter(error) to continue to errorHandler middleware:
+//     .catch(error => next(error))
+// })
 
 
-app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
+// app.delete('/api/persons/:id', (request, response, next) => {
+//   Person.findByIdAndRemove(request.params.id)
+//     .then(result => {
+//       response.status(204).end()
+//     })
+//     .catch(error => next(error))
+// })
 
 
-app.post('/api/persons', (request, response, next) => {
-  // get new person from request object
-  const body = request.body
-  // create new person object using Note constructor function
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
-  // add to persons array
-  person.save()
-    .then(savedPerson => {
-      response.json(savedPerson)
-    })
-    .catch(error => next(error))
-})
+// app.post('/api/persons', (request, response, next) => {
+//   // get new person from request object
+//   const body = request.body
+//   // create new person object using Note constructor function
+//   const person = new Person({
+//     name: body.name,
+//     number: body.number,
+//   })
+//   // add to persons array
+//   person.save()
+//     .then(savedPerson => {
+//       response.json(savedPerson)
+//     })
+//     .catch(error => next(error))
+// })
 
 
-app.put('/api/persons/:id', (request, response, next) => {
-  const { name, number } = request.body
+// app.put('/api/persons/:id', (request, response, next) => {
+//   const { name, number } = request.body
 
-  Person.findByIdAndUpdate(
-    request.params.id,
-    { name, number },
-    { new: true, runValidators: true, context: 'query' }
-  )
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
-})
+//   Person.findByIdAndUpdate(
+//     request.params.id,
+//     { name, number },
+//     { new: true, runValidators: true, context: 'query' }
+//   )
+//     .then(updatedPerson => {
+//       response.json(updatedPerson)
+//     })
+//     .catch(error => next(error))
+// })
 
 
-app.get('/info', (request, response, next) => {
-  Person.count()
-    .then(count => {
-      response.json(
-        { NumberOfPersons:count,
-          date:Date()
-        }
-      )
-    })
-    .catch((error) => next(error) )
-  //response.send(`<p> Phonebook has info for ${size} people </p> <p> ${Date()} </p>`)
-})
+// app.get('/info', (request, response, next) => {
+//   Person.count()
+//     .then(count => {
+//       response.json(
+//         { NumberOfPersons:count,
+//           date:Date()
+//         }
+//       )
+//     })
+//     .catch((error) => next(error) )
+// })
+
+
 
 
 const unknownEndpoint = (request, response) => {
