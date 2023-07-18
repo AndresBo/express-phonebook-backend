b0973200560142bd3dba372e1fb149bbb7f4ff2f
AndresBo <anndresfborrero@gmail.com>
2023-07-19 05:36:09 +1000

    fix: modified logger module so it does not print to console when in test mode

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,15 @@
 
 // function that prints normal log messages:
 const info = (...params) => {
-  console.log(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(...params)
+  }
 }
 // function that prints error messages:
 const error = (...params) => {
-  console.error(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(...params)
+  }
 }
 
 module.exports = {
